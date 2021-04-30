7765092f7c071f6b7b0daa61c2e559356ac623c6
riag <riag@163.com>
2021-04-30 09:51:28 +0800

    增加检查错误码是否有重复定义的

--- errorcode_generator.py
+++ errorcode_generator.py
@@ -12,30 +12,55 @@ from typing import List, TextIO
 
 class ErrorCode(object):
 
-    def __init__(self, d:dict):
+    def __init__(self, fpath:str, d:dict):
         self.comment:str = d.get("comment", "")
         self.code:str = d.get("code", "")
         self.name:str = d.get("name", "")
+        # 错误码是定义在哪里文件里
+        self.fpath = fpath
 
     def is_comment(self):
         if len(self.name) == 0 or len(self.code) == 0:
             return True
         return False
 
-def default_handle(fpath:str, ec_list:List[ErrorCode], item:dict):
-    ec = ErrorCode(item)
-    ec_list.append(ec)
+class ErrorCodeContext(object):
+    def __init__(self):
+        self.error_codes = []
+        self.code_map = {}
+        self.name_map = {}
 
-def include_handle(fpath:str, ec_list:List[ErrorCode], item:dict):
+    def add_error_code(self, error_code: ErrorCode):
+        if not error_code.is_comment():
+            if error_code.code in self.code_map:
+                raise Exception(
+                    "error code [%s] already define in %s" % (error_code.code, error_code.fpath)
+                    )
+            if error_code.name in self.name_map:
+                raise Exception(
+                    "error code [%s] already define in %s" % (error_code.name, error_code.fpath)
+                    )
+
+            self.code_map[error_code.code] = error_code
+            self.name_map[error_code.name] = error_code
+
+        self.error_codes.append(error_code)
+
+
+def default_handle(context: ErrorCodeContext, parentdir:str, fpath:str, item:dict):
+    ec = ErrorCode(fpath, item)
+    context.add_error_code(ec)
+
+def include_handle(context: ErrorCodeContext, parentdir:str, fpath:str, item:dict):
     v = item.get("include", None)
     if v is None:
         return False
 
     p = v
     if not os.path.isabs(p):
-        p = os.path.abspath(os.path.join(fpath, p))
-    l = parse_errorcodes(p)
-    ec_list.extend(l)
+        p = os.path.abspath(os.path.join(parentdir, p))
+
+    parse_errorcodes(context, p)
 
     return True
 
@@ -45,19 +70,18 @@ handle_func = [
 ]
 
 
-def parse_errorcodes(fpath:str) -> List[ErrorCode]:
+def parse_errorcodes(context: ErrorCodeContext, fpath:str) :
     data:str= ""
-    pardir = os.path.dirname(fpath)
+    parentdir = os.path.dirname(fpath)
+
     with open(fpath, 'r') as f:
         data = f.read()
+
     l = json.loads(data) 
-    ec_list:List[ErrorCode] = []
     for item in l:
         for func in handle_func:
-            if func(pardir, ec_list, item):
+            if func(context, parentdir, fpath, item):
                 break
-
-    return ec_list
 
 
 def render_str(tmp_str:str, ec_list: List[ErrorCode], out : TextIO, args: Namespace) -> None:
@@ -114,13 +138,15 @@ tmp_map = {
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--type", default="markdown", help="buildin render type")
+    parser.add_argument("-t", "--type", default="markdown", help="buildin render type, support: markdown, asciidoc, rst")
     parser.add_argument("--tpl", help="jinja2 template file")
     parser.add_argument("--out", help="out file")
     parser.add_argument("errorcode_file", help="define error code in this file")
     args: Namespace = parser.parse_args()
 
-    ec_list = parse_errorcodes(args.errorcode_file)
+    p = os.path.abspath(args.errorcode_file)
+    context = ErrorCodeContext()
+    parse_errorcodes(context, p)
     out:TextIO = sys.stdout
     f = None
     if args.out is not None:
@@ -139,7 +165,7 @@ def cli():
             with open(args.tpl, 'r') as f:
                 tmp = f.read()
 
-        render_str(tmp, ec_list, out, args)
+        render_str(tmp, context.error_codes, out, args)
     finally:
         if f is not None:
             f.close()
